8ec2316464275c636fa60494470fab6f58e89d7b
Brett Scheller <brett.green.scheller@gmail.com>
2018-10-30 15:34:16 -0500

    add new product component

---
+++ client/components/AddProduct.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {postProduct} from '../store'
+
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import MenuItem from '@material-ui/core/MenuItem'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing.unit
+  }
+})
+
+class AddProduct extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      price: null,
+      description: '',
+      imgUrl: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.postNewProduct(this.state)
+    this.setState({
+      name: '',
+      price: '',
+      description: '',
+      imgUrl: ''
+    })
+  }
+  render() {
+    const {classes} = this.props
+    console.log(this.props)
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          name="name"
+          id="outlined-name"
+          label="Name"
+          className={classes.textField}
+          value={this.state.name}
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          name="description"
+          id="outlined-name"
+          label="Description"
+          className={classes.textField}
+          value={this.state.description}
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <Button
+          onClick={this.handleSubmit}
+          variant="outlined"
+          className={classes.button}
+        >
+          Submit
+        </Button>
+      </form>
+    )
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    postNewProduct: product => {
+      dispatch(postProduct(product))
+    }
+  }
+}
+
+AddProduct.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(connect(null, mapDispatch)(AddProduct))
