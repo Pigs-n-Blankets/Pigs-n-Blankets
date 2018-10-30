bfcbe9d242053592718389c40845906d632e3608
Daniel Eimer <danieleimer1@gmail.com>
2018-10-30 15:55:28 -0500

    navbar progress

--- client/components/Loading.js
+++ client/components/Loading.js
@@ -4,6 +4,7 @@ export const Loading = () => {
   return (
     <div>
       <h1>Loading...</h1>
+      <img src="https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif" />
     </div>
   )
 }
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,29 +4,199 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>PIGS 'N BLANKETS HIIIII</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+import Badge from '@material-ui/core/Badge'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import {fade} from '@material-ui/core/styles/colorManipulator'
+import {withStyles} from '@material-ui/core/styles'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import MoreIcon from '@material-ui/icons/MoreVert'
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing.unit * 2,
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 3,
+      width: 'auto'
+    }
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%'
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200
+    }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    }
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
+    }
+  }
+})
+
+const handleSubmit = event => {
+  event.preventDefault()
+  const searchTerm = event.target.value
+  alert('CONFIGURE ME IN HANDLESUBMIT IN NAVBAR.JS')
+}
+
+const Navbar = ({classes, handleClick, isLoggedIn}) => {
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to="/">
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Pigs 'N Blankets
+            </Typography>
+          </Link>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+            />
+          </div>
+          <Link to="/products">
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              Products
+            </Typography>
+          </Link>
+          <div className={classes.grow} />
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/cart">
+                <IconButton color="inherit">
+                  <Badge badgeContent={4} color="secondary">
+                    <ShoppingCartIcon />
+                  </Badge>
+                </IconButton>
+              </Link>
+              <Link to="/account">
+                <IconButton aria-haspopup="true" color="inherit">
+                  <AccountCircle />
+                </IconButton>
+              </Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+// const Navbar = ({classes, handleClick, isLoggedIn}) => {
+//   return (
+//     <div className={classes.root}>
+//       <nav>
+//         <Link to="/">Pigs 'N Blankets</Link>
+//         <form onSubmit={handleSubmit}>
+//           <input name="search" type="text" defaultValue="Search" />
+//           <button type="submit">Search!</button>
+//         </form>
+//         <Link to="/products">Products</Link>
+//         {isLoggedIn ? (
+//           <div>
+//             {/* The navbar will show these links after you log in */}
+//             <Link to="/account">My Account</Link>
+//             <Link to="/cart">My Cart</Link>
+//             <a href="#" onClick={handleClick}>
+//               Logout
+//             </a>
+//           </div>
+//         ) : (
+//           <div>
+//             {/* The navbar will show these links before you log in */}
+//             <Link to="/login">Login</Link>
+//             <Link to="/signup">Sign Up</Link>
+//           </div>
+//         )}
+//       </nav>
+//     </div>
+//   )
+// }
 
 /**
  * CONTAINER
@@ -45,12 +215,13 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
-
+export default withStyles(styles)(connect(mapState, mapDispatch)(Navbar))
 /**
  * PROP TYPES
  */
+
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
