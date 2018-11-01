b58037f337d4b89b6106da4b67ff26aba3046794
Daniel Eimer <danieleimer1@gmail.com>
2018-11-01 18:29:35 -0500

    added express route - untested

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,7 +4,7 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   let id
-  let idType;
+  let idType
   if (req.user) {
     id = req.user.dataValues.id
     idType = 'userId'
@@ -14,16 +14,48 @@ router.get('/', async (req, res, next) => {
   }
 
   try {
-      const orders = await Order.findAll({
-        include: [{
+    const orders = await Order.findAll({
+      include: [
+        {
           model: Product
-        }],
-        where: {
-          [idType]: id,
-          orderStatus: 'inCart'
         }
-      })
-      res.json(orders)
+      ],
+      where: {
+        [idType]: id,
+        orderStatus: 'inCart'
+      }
+    })
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  let id
+  let idType
+  if (req.user) {
+    id = req.user.dataValues.id
+    idType = 'userId'
+  } else {
+    id = req.session.id
+    idType = 'sessionId'
+  }
+  const price = req.body.price
+  const quantity = req.body.quantity
+  const subtotal = quantity * price
+  const product = req.body.product
+  const myOrder = {
+    [idType]: id,
+    orderStatus: 'inCart',
+    price: price,
+    quantity: quantity,
+    subtotal: subtotal
+  }
+  try {
+    const order = await Order.create(myOrder)
+    await order.setProduct(product)
+    res.json(order)
   } catch (err) {
     next(err)
   }
