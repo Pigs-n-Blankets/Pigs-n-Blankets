7493fee256d424eb662bf9a56bca73c27c36c426
Julianne Crawford <julianne.marik@gmail.com>
2018-11-01 20:59:55 -0500

    cart component styling

--- client/components/cart/CartCard.js
+++ client/components/cart/CartCard.js
@@ -1,46 +1,100 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 // MATERIAL UI IMPORTS
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import ImageIcon from '@material-ui/icons/Image';
-import Divider from '@material-ui/core/Divider';
-
+import {withStyles} from '@material-ui/core/styles'
+import TableCell from '@material-ui/core/TableCell'
+import TableRow from '@material-ui/core/TableRow'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const styles = theme => ({
   root: {
-    // display: 'flex',
-    // justifyContent: 'center',
     width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
   },
+  productImg: {
+    maxWidth: '100%'
+  },
+  imgCell: {
+    width: '25%'
+  },
+  nameCell: {
+    width: '25%',
+    textAlign: 'right'
+  },
+  priceCell: {
+    width: '15%'
+  },
+  quantityCell: {
+    width: '25%',
+    fontSize: '10px'
+  },
+  removeCell: {
+    width: '5%',
+    textAlign: 'right'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '40%',
+    height: 'auto'
+  }
+})
 
-});
+class CartCard extends Component {
+  state = {
+    quantity: '1'
+  }
 
-function CartCard(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-        <List>
-          <ListItem>
-            <Avatar>
-              <ImageIcon />
-            </Avatar>
-            <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-          </ListItem>
-          <li>
-            <Divider inset />
-          </li>
-        </List>
-    </div>
-  );
+  handleChange = () => {
+    this.setState({
+      quantity: event.target.value
+    })
+  }
+  render() {
+    const {classes, order} = this.props
+    const {product} = order
+    const {imgUrl, name, price, quantity} = product
+    return (
+      <TableRow>
+        <TableCell className={classes.imgCell}>
+          <img src={imgUrl} className={classes.productImg} />
+        </TableCell>
+        <TableCell className={classes.nameCell}>
+          <Typography variant="subtitle1">{name}</Typography>
+        </TableCell>
+        <TableCell numeric className={classes.priceCell}>
+          <Typography variant="subtitle1">{`$${price}`}</Typography>
+        </TableCell>
+        <TableCell numeric className={classes.quantityCell}>
+          <TextField
+            required
+            className={classes.textField}
+            value={this.state.quantity}
+            onChange={this.handleChange}
+            margin="normal"
+            variant="outlined"
+          />
+        </TableCell>
+        <TableCell className={classes.removeCell}>
+          <Button
+            type="button"
+            className={classes.submit}
+          >
+            <DeleteIcon className={classes.rightIcon} />
+          </Button>
+        </TableCell>
+      </TableRow>
+    )
+  }
 }
 
 CartCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+  classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(CartCard);
+export default withStyles(styles)(CartCard)
--- client/components/cart/CartView.js
+++ client/components/cart/CartView.js
@@ -5,19 +5,35 @@ import CartCard from './CartCard'
 
 // MATERIAL UI IMPORTS
 import {withStyles} from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableFooter from '@material-ui/core/TableFooter'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 
 const style = theme => ({
-  root: {
-    display: 'flex',
+  wrapper: {
     width: '100%',
-    justifyContent: 'center',
+    display: 'flex',
+    justifyContent: 'center'
   },
   content: {
+    width: '70%',
     display: 'flex',
     flexDirection: 'column',
-    // alignItems: 'center',
-    width: '70%',
+    alignItems: 'center'
+  },
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 2,
+    overflowX: 'auto',
+  },
+  table: {
+    width: '100%'
   },
   submit: {
     marginTop: theme.spacing.unit,
@@ -34,11 +50,35 @@ class CartView extends Component {
   render() {
     const {classes} = this.props
     return (
-      <div className={classes.root}>
+      <div className={classes.wrapper}>
         <div className={classes.content}>
-          {this.props.cart.map(order => {
-            return <CartCard key={order.id} />
-          })}
+          <Paper className={classes.root}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell />
+                  <TableCell numeric>PRODUCT</TableCell>
+                  <TableCell numeric>PRICE</TableCell>
+                  <TableCell numeric>QUANTITY</TableCell>
+                  <TableCell numeric>REMOVE</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.props.cart.map(order => {
+                  return <CartCard key={order.id} order={order} />
+                })}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell />
+                  <TableCell />
+                  <TableCell numeric>Subtotal</TableCell>
+                  <TableCell numeric>quantity</TableCell>
+                  <TableCell />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </Paper>
           <Button
             type="button"
             variant="contained"
