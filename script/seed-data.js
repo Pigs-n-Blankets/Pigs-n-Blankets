796e3b7b3b451cfd09450dd33badbf233fba7f49
Julianne Crawford <julianne.marik@gmail.com>
2018-11-02 08:34:39 -0500

    seed-data file added

---
+++ script/seed-data.js
@@ -0,0 +1,122 @@
+const productData = [
+  {
+    name: 'Doggy Snowman',
+    price: 10.99,
+    description: 'dress your dog up as a snowman this holiday season!',
+    rating: 4.7,
+    imgUrl:
+      'https://www.rover.com/blog/wp-content/uploads/2017/12/snowmandog.jpg'
+  },
+  {
+    name: 'Dapper Pup',
+    price: 7.95,
+    description: 'have the most dapper pup with this puppy tuxedo!',
+    rating: 3.4,
+    imgUrl:
+      'https://image.dhgate.com/0x0/f2/albu/g4/M00/81/F0/rBVaEFds4Z-AaBqjAAams5cUse8326.jpg'
+  },
+  {
+    name: 'Super Cat',
+    price: 14.75,
+    description: 'We all know cats are the SUPERior species',
+    rating: 4.9,
+    imgUrl:
+      'http://cutecatsinhats.com/wp-content/uploads/2016/03/Gaorui-Puppy-lovely-Superman-Pet-Dog-Clothes-Halloween-apparel-Costumes-Cute-Jumpsuit-0-1024x1024.jpg'
+  },
+  {
+    name: 'Sassy Kitty',
+    price: 13.99,
+    rating: 2.3,
+    imgUrl:
+      'https://ae01.alicdn.com/kf/HTB1xAzFNpXXXXbMXpXXq6xXFXXX9/Pet-Supplies-Cat-Clothes-Cats-Wear-Pet-Coats-Hoopet-Clothing-For-Domestic-Cats-Blouse-Jackets-Dogs.jpg'
+  },
+  {
+    name: 'Cowboy Piglet',
+    price: 17.8,
+    rating: 5,
+    description: 'I mean, how can you resist...?',
+    imgUrl:
+      'http://thefw.com/files/2012/09/tumblr_m0edolFcz51rr5f7co1_500-e1348601887820.jpg?w=630&h=630&q=75'
+  },
+  {
+    name: 'Piglet Booties',
+    price: 24.95,
+    rating: 2.7,
+    description: 'keep your piglets hooves dry in its pen',
+    imgUrl:
+      'http://slummysinglemummy.files.wordpress.com/2012/03/pig-in-wellies.jpg'
+  },
+  {
+    name: 'Identity Crisis',
+    price: 24.95,
+    rating: 2.7,
+    description:
+      'Do you have a pig who thinks he is a unicorn? This is the perfect compromise',
+    imgUrl:
+      'https://i.dailymail.co.uk/i/pix/2014/09/26/1411729138384_Image_galleryImage_PIC_FROM_CATERS_NEWS_PICT.JPG'
+  },
+]
+
+const userData = [
+  {
+    firstName: 'Cody',
+    lastName: 'TheDog',
+    email: 'cody@email.com',
+    address: 'Doghouse Chicago, IL',
+    password: '123'
+  },
+  {
+    firstName: 'Murphy',
+    lastName: 'TheDog',
+    email: 'murphy@email.com',
+    address: 'Doghouse Chicago, IL',
+    password: '123'
+  }
+]
+
+const orderData = [
+
+]
+
+const reviewData = [
+  {
+    rating: 3,
+    description: 'its alright'
+  },
+  {
+    rating: 2,
+    description: 'its bad'
+  },
+  {
+    rating: 4,
+    description: 'its nice now'
+  },
+  {
+    rating: 1,
+    description: 'its really really bad'
+  },
+  {
+    rating: 2.5,
+    description: 'its ehh'
+  },
+  {
+    rating: 5,
+    description: 'wow!'
+  },
+]
+
+const categoryData = [
+  {name: 'dogs'},
+  {name: 'cats'},
+  {name: 'pigs'},
+  {name: 'horses'},
+]
+
+
+module.exports = {
+  productData,
+  userData,
+  reviewData,
+  categoryData,
+  orderData,
+};
