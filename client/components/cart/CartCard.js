bc1d8b6c20fe16b4c1f774ea044b331a0d29416f
Julianne Crawford <julianne.marik@gmail.com>
2018-11-02 22:59:32 -0500

    added product seed data

--- client/components/cart/CartCard.js
+++ client/components/cart/CartCard.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {deleteFromCart} from '../../store'
 import {Link} from 'react-router-dom'
 import {deleteFromCart, replaceCartQuantity} from '../../store'
 
