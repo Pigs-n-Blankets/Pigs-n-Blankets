1285d288f2fb5756b967ad19e7ff481074697885
Julianne Crawford <julianne.marik@gmail.com>
2018-11-04 21:23:38 -0600

    link added to menu

--- client/routes.js
+++ client/routes.js
@@ -49,7 +49,7 @@ class Routes extends Component {
             <Route path="/review/:productId" component={ReviewForm} />
             <Route path="/admin/orders" component={Orders} />
             <Route path="/admin" component={Dashboard} />
-      
+
             {/* move to isAdmin */}
             <Route path="/allUsers" component={AllUsers} />
 
@@ -75,7 +75,7 @@ const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.currentUser.id
+    isLoggedIn: !!state.user.currentUser.id,
     isAdmin: state.user.isAdmin
   }
 }
