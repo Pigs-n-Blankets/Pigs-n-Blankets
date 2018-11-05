6806149f81b9780a4e45b7702b90353e120033ad
Julianne Crawford <julianne.marik@gmail.com>
2018-11-05 13:10:05 -0600

    button to add product added

--- client/components/product/AddProduct.js
+++ client/components/product/AddProduct.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {postProduct} from '../../store'
+import {Link} from 'react-router-dom'
 
+// MATERIAL UI IMPORTS
 import {withStyles} from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
@@ -70,6 +72,14 @@ class AddProduct extends Component {
   handleSubmit(event) {
     event.preventDefault()
     this.props.postProduct(this.state)
+    this.setState({
+      name: '',
+      price: '',
+      description: '',
+      rating: 1,
+      imgUrl: '',
+      inventory: 0
+    })
   }
 
   render() {
@@ -164,14 +174,11 @@ class AddProduct extends Component {
             </GridList>
           </CardContent>
           <CardActions className={classes.cardActions}>
-            <Button
+          <Button
               type="button"
-              // onClick={() => {this.handleSubmit(event)}}
-              // color="primary"
-              // className={classes.submit}
               onClick={this.handleSubmit}
             >
-              Add Product
+              <Link to="/products">Add Product</Link>
             </Button>
           </CardActions>
         </Card>
@@ -179,12 +186,6 @@ class AddProduct extends Component {
     )
   }
 }
-
-// const mapState = state => {
-//   return {
-//     user: state.user.currentUser
-//   }
-// }
 
 const mapDispatch = dispatch => {
   return {
@@ -195,111 +196,3 @@ const mapDispatch = dispatch => {
 }
 
 export default withStyles(styles)(connect(null, mapDispatch)(AddProduct))
-
-// import React, {Component} from 'react'
-// import {connect} from 'react-redux'
-// import {postProduct} from '../../store'
-
-// import PropTypes from 'prop-types'
-// import {withStyles} from '@material-ui/core/styles'
-// import MenuItem from '@material-ui/core/MenuItem'
-// import TextField from '@material-ui/core/TextField'
-// import Button from '@material-ui/core/Button'
-
-// const styles = theme => ({
-//   container: {
-//     display: 'flex',
-//     flexWrap: 'wrap'
-//   },
-//   textField: {
-//     marginLeft: theme.spacing.unit,
-//     marginRight: theme.spacing.unit
-//   },
-//   dense: {
-//     marginTop: 16
-//   },
-//   menu: {
-//     width: 200
-//   },
-//   button: {
-//     margin: theme.spacing.unit
-//   }
-// })
-
-// class AddProduct extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       name: undefined,
-//       price: undefined,
-//       description: undefined,
-//       imgUrl: undefined
-//     }
-//     this.handleChange = this.handleChange.bind(this)
-//     this.handleSubmit = this.handleSubmit.bind(this)
-//   }
-//   handleChange(event) {
-//     this.setState({
-//       [event.target.name]: event.target.value
-//     })
-//   }
-//   handleSubmit(event) {
-//     event.preventDefault()
-//     this.props.postNewProduct(this.state)
-//     this.setState({
-//       name: null,
-//       price: null,
-//       description: null,
-//       imgUrl: null
-//     })
-//   }
-//   render() {
-//     const {classes} = this.props
-//     return (
-//       <form className={classes.container} noValidate autoComplete="off">
-//         <TextField
-//           name="name"
-//           id="outlined-name"
-//           label="Name"
-//           className={classes.textField}
-//           value={this.state.name}
-//           onChange={this.handleChange}
-//           margin="normal"
-//           variant="outlined"
-//         />
-//         <TextField
-//           name="description"
-//           id="outlined-name"
-//           label="Description"
-//           className={classes.textField}
-//           value={this.state.description}
-//           onChange={this.handleChange}
-//           margin="normal"
-//           variant="outlined"
-//         />
-//         <Button
-//           type="Submit"
-//           onClick={this.handleSubmit}
-//           variant="outlined"
-//           className={classes.button}
-//         >
-//           Submit
-//         </Button>
-//       </form>
-//     )
-//   }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     postNewProduct: product => {
-//       dispatch(postProduct(product))
-//     }
-//   }
-// }
-
-// AddProduct.propTypes = {
-//   classes: PropTypes.object.isRequired
-// }
-
-// export default withStyles(styles)(connect(null, mapDispatch)(AddProduct))
